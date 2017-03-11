03dd425f2997b321f5640629fa0a85f759f3636d
DongBinChoiTemple <tuf73966@temple.edu>
2017-03-11 18:44:04 -0500

    Created log in modal

--- VoiceTest/app/src/main/java/edu/temple/voicetest/MainActivity.java
+++ VoiceTest/app/src/main/java/edu/temple/voicetest/MainActivity.java
@@ -8,6 +8,9 @@ import android.speech.RecognizerIntent;
 import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -19,8 +22,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displaySpeechRecognizer();
+        ((Button)findViewById(R.id.button_signup)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ((Button)findViewById(R.id.button_login)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                LoginDialogFragment frag = LoginDialogFragment.newInstance("Log In");
+                frag.show(getFragmentManager(), "dialog");
+            }
+        });
     }
+
+
+    public static class LoginDialogFragment extends DialogFragment {
+        public static LoginDialogFragment newInstance(String title) {
+            LoginDialogFragment frag = new LoginDialogFragment();
+            Bundle args = new Bundle();
+            args.putString("title", title);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            // Get the layout inflater
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            String title = getArguments().getString("title");
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(inflater.inflate(R.layout.dialog_login, null))
+                    // Add action buttons
+                    .setTitle(title)
+                    .setPositiveButton(R.string.login, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int id) {
+                            // sign in the user ...
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            LoginDialogFragment.this.getDialog().cancel();
+                        }
+                    });
+            return builder.create();
+        }
+    }
+
+    /*
     private static final int SPEECH_REQUEST_CODE = 0;
 
     // Create an intent that can start the Speech Recognizer activity
@@ -28,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-// Start the activity, the intent will be populated with the speech text
+    // Start the activity, the intent will be populated with the speech text
         startActivityForResult(intent, SPEECH_REQUEST_CODE);
     }
 
@@ -85,4 +139,5 @@ public class MainActivity extends AppCompatActivity {
                     .create();
         }
     }
+    */
 }
